16b41c1b5f3adc50559b98de4466485ca4909dd5
Ahsaan Ullah <ahsaanu70@gmail.com>
2024-07-13 11:14:35 +0500

    find maximum and minimum number using array using max and min function

--- array.cpp
+++ array.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 int main(){
-int n ;
-cin>>n;
-int array[n];
-for(int i=0;i<n;i++){
-    cin>>array[i];
+    int maxNo = INT_MIN;
+    int minNo = INT_MAX;
+int arr[4] = {1,2,3,4};
+// for(int i=0;i<n;i++){
+//     cin>>array[i];
+// }
+// for(int i=0;i<n;i++){
+//     cout<<array[i]<<" ";
+// }
+
+for(int i=0;i<4;i++){
+    maxNo = max(maxNo,arr[i]);
+    minNo = min(minNo,arr[i]);
 }
-for(int i=0;i<n;i++){
-    cout<<array[i]<<" ";
-}
+cout<<maxNo<<endl;
+cout<<minNo<<endl;
 }
