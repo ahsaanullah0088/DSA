2f5546c6293295bc97657596e7053bc094f1635c
Ahsaan Ullah <ahsaanu70@gmail.com>
2024-07-13 11:37:22 +0500

    binary search in c++

--- array8.2searchelementsin.cpp
+++ array8.2searchelementsin.cpp
@@ -1,24 +1,56 @@
 #include<iostream>
 using namespace std;
-int linearSearch(int arr[],int n, int key){
-    for(int i=0; i<n ; i++){
-        if(arr[i] == key){
-            return i;
+int binarySearch(int arr[], int n, int key){
+    int s=0;
+    int e=n;
+    while(s<=e){
+        int mid = (s+e)/2;
+        if(arr[mid] == key){
+            return mid;
+        }
+        else if(arr[mid] > key){
+            e = mid-1;
+        }
+        else{
+            s = mid+1;
         }
     }
     return -1;
 }
+
+// int linearSearch(int arr[],int n, int key){
+//     for(int i=0; i<n ; i++){
+//         if(arr[i] == key){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
 int main(){
+    // binary search
     int n;
     cout<<"Enter the size of the array: ";
     cin>>n;
-    int array[n];
+    int arr[n];
     for(int i=0;i<n;i++){
-        cin>>array[i];
+        cin>>arr[i];
     }
     int key;
     cout<<"Enter the key: ";
     cin>>key;
-    cout<<linearSearch(array,n,key)<<endl;
+    cout<<binarySearch(arr,n,key)<<endl;
+
+
+    // int n;
+    // cout<<"Enter the size of the array: ";
+    // cin>>n;
+    // int array[n];
+    // for(int i=0;i<n;i++){
+    //     cin>>array[i];
+    // }
+    // int key;
+    // cout<<"Enter the key: ";
+    // cin>>key;
+    // cout<<linearSearch(array,n,key)<<endl;
 
 }
