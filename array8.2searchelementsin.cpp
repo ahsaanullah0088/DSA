713aeee565a709e204ae584f069046c095120a7c
Ahsaan Ullah <ahsaanu70@gmail.com>
2024-07-13 11:21:07 +0500

    find element in an array

---
+++ array8.2searchelementsin.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int linearSearch(int arr[],int n, int key){
+    for(int i=0; i<n ; i++){
+        if(arr[i] == key){
+            return i;
+        }
+    }
+    return -1;
+}
+int main(){
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    int array[n];
+    for(int i=0;i<n;i++){
+        cin>>array[i];
+    }
+    int key;
+    cout<<"Enter the key: ";
+    cin>>key;
+    cout<<linearSearch(array,n,key)<<endl;
+
+}
