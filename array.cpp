57c67b7163e80b19e1d04b3302d242c829a90a8e
Ahsaan Ullah <ahsaanu70@gmail.com>
2024-07-13 11:07:28 +0500

    get array values from user

--- array.cpp
+++ array.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(){
-      int array[4];
-        array[0] = 1;
-        array[1] = 2;
-        array[2] = 3;
-        array[3] = 4;
-        cout<<array[0]<<endl;
+int n ;
+cin>>n;
+int array[n];
+for(int i=0;i<n;i++){
+    cin>>array[i];
+}
+for(int i=0;i<n;i++){
+    cout<<array[i]<<" ";
+}
 }
